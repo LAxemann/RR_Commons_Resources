62e03a089a734c70b358470bfc9e2a55b6bd0ad7
Wasserstoff <41219647+GermanHydrogen@users.noreply.github.com>
2023-01-31 20:28:26 +0100

    Added CBA settings for KAT from 2.KdpKp

---
+++ addons/main/modKonfigurationsTemplates/medicSettings/kat.h
@@ -0,0 +1,163 @@
+/**
+ * KAT settings from 2.KdpKp
+ *
+*/
+
+// KAT - ADV Medical: Airway
+force force kat_airway_Accuvac_time = 4;
+force force kat_airway_CancelRecoveryPosition_Time = 6;
+force force kat_airway_CheckAirway_time = 1;
+force force kat_airway_checkbox_puking_sound = true;
+force force kat_airway_Colored_logs = true;
+force force kat_airway_enable = true;
+force force kat_airway_Guedeltubus_time = 3;
+force force kat_airway_Larynxtubus_time = 2;
+force force kat_airway_medLvl_Accuvac = 1;
+force force kat_airway_medLvl_Guedeltubus = 0;
+force force kat_airway_medLvl_Larynxtubus = 1;
+force force kat_airway_occlusion_repeatTimer = 60;
+force force kat_airway_Overstretch_time = 1.5;
+force force kat_airway_probability_headturning = 50;
+force force kat_airway_probability_obstruction = 15;
+force force kat_airway_probability_occluded = 10;
+force force kat_airway_RecoveryPosition_Time = 6;
+force force kat_airway_string_exit = "";
+force force kat_airway_TurnAround_time = 1;
+
+// KAT - ADV Medical: Breathing
+force kat_breathing_advPtxChance = 5;
+force kat_breathing_advPtxEnable = false;
+force force kat_breathing_deterioratingPneumothorax_chance = 10;
+force force kat_breathing_deterioratingPneumothorax_countdown = 120;
+force force kat_breathing_enable = true;
+force force kat_breathing_enable_selfChestseal = 1;
+force force kat_breathing_enableCyanosis = true;
+force kat_breathing_hptxChance = 5;
+force force kat_breathing_medLvl_Chestseal = 0;
+force force kat_breathing_medLvl_Cyanosis = 0;
+force force kat_breathing_medLvl_hemopneumothoraxTreatment = 1;
+force force kat_breathing_medLvl_Pulseoximeter = 0;
+force force kat_breathing_mildValue = 75;
+force force kat_breathing_pneumothorax_hardcore = false;
+force kat_breathing_pneumothoraxChance = 5;
+force force kat_breathing_pneumothoraxDamageThreshold = 0.4;
+force force kat_breathing_severeValue = 66;
+force force kat_breathing_showPneumothorax_dupe = false;
+force force kat_breathing_slightValue = 90;
+force force kat_breathing_SpO2_dieActive = false;
+force force kat_breathing_SpO2_dieValue = 65;
+force force kat_breathing_SpO2_MultiplyNegative = 1;
+force force kat_breathing_SpO2_MultiplyPositive = 3;
+force force kat_breathing_SpO2_perfusion = true;
+force force kat_breathing_SpO2_PerfusionMultiplier = 1;
+force force kat_breathing_SpO2_unconscious = 75;
+force force kat_breathing_Stable_spo2 = 85;
+force force kat_breathing_tensionhemothorax_hardcore = false;
+
+// KAT - ADV Medical: Circulation
+force force kat_circulation_AdvRhythm = true;
+force force kat_circulation_AED_BeepsAndCharge = true;
+force force kat_circulation_AED_MaxChance = 80;
+force force kat_circulation_AED_MinChance = 40;
+force force kat_circulation_AED_X_MaxChance = 90;
+force force kat_circulation_AED_X_MinChance = 45;
+force force kat_circulation_blood_draw_limit = 3.6;
+force force kat_circulation_blood_drawTime_250ml = 15;
+force force kat_circulation_blood_drawTime_500ml = 25;
+kat_circulation_bloodgroup = "O_N";
+force force kat_circulation_bloodGroups = true;
+force force kat_circulation_CPR_MaxChance_Default = 20;
+force force kat_circulation_CPR_MaxChance_Doctor = 40;
+force force kat_circulation_CPR_MaxChance_RegularMedic = 30;
+force force kat_circulation_CPR_MinChance_Default = 10;
+force force kat_circulation_CPR_MinChance_Doctor = 20;
+force force kat_circulation_CPR_MinChance_RegularMedic = 15;
+force force kat_circulation_DeactMon_whileAED_X = true;
+force force kat_circulation_distanceLimit_AEDX = 30;
+force force kat_circulation_enable = true;
+force force kat_circulation_enable_CPR_Chances = true;
+force force kat_circulation_enable_selfBloodDraw = 1;
+force force kat_circulation_medLvl_AED = 0;
+force force kat_circulation_medLvl_AED_X = 0;
+force force kat_circulation_timeLimit_AEDX = 1800;
+force force kat_circulation_useLocation_AED = 0;
+
+// KAT - ADV Medical: Misc
+force force kat_misc_arsenalMedCategory = true;
+force force kat_misc_enable = true;
+force force kat_misc_incompatibilityWarning = true;
+
+// KAT - ADV Medical: Pharmacy
+force force kat_pharma_blockChance = 20;
+force force kat_pharma_carbonateChance = 100;
+force force kat_pharma_coagulation = true;
+force force kat_pharma_ivCheckLimbDamage = false;
+force force kat_pharma_IVdrop = 600;
+force force kat_pharma_IVdropEnable = true;
+force force kat_pharma_IVreuse = false;
+force force kat_pharma_kidneyAction = false;
+force force kat_pharma_MedicationsRequireInsIV = false;
+force force kat_pharma_medLvl_Amiodarone = 0;
+force force kat_pharma_medLvl_ApplyIO = 0;
+force force kat_pharma_medLvl_ApplyIV = 0;
+force force kat_pharma_medLvl_Atropine = 0;
+force force kat_pharma_medLvl_Carbonate = 0;
+force force kat_pharma_medLvl_EACA = 0;
+force force kat_pharma_medLvl_Etomidate = 0;
+force force kat_pharma_medLvl_Fentanyl = 0;
+force force kat_pharma_medLvl_Flumezenil = 0;
+force force kat_pharma_medLvl_Ketamine = 0;
+force force kat_pharma_medLvl_Lidocaine = 0;
+force force kat_pharma_medLvl_Lorazepam = 0;
+force force kat_pharma_medLvl_Nalbuphine = 0;
+force force kat_pharma_medLvl_Naloxone = 0;
+force force kat_pharma_medLvl_Nitroglycerin = 0;
+force force kat_pharma_medLvl_Norepinephrine = 0;
+force force kat_pharma_medLvl_Phenylephrine = 0;
+force force kat_pharma_medLvl_Reorientation = 0;
+force force kat_pharma_medLvl_TXA = 0;
+force force kat_pharma_Reorientation_Enable = true;
+force force kat_pharma_Reorientation_Slap = true;
+force force kat_pharma_reorientationChance = 100;
+force force kat_pharma_RequireInsIV = true;
+force force kat_pharma_treatmentTime_Amiodarone = 3.5;
+force force kat_pharma_treatmentTime_ApplyIO = 3.5;
+force force kat_pharma_treatmentTime_ApplyIV = 3.5;
+force force kat_pharma_treatmentTime_Atropine = 3.5;
+force force kat_pharma_treatmentTime_Carbonate = 3.5;
+force force kat_pharma_treatmentTime_EACA = 3.5;
+force force kat_pharma_treatmentTime_Etomidate = 2;
+force force kat_pharma_treatmentTime_Fentanyl = 3.5;
+force force kat_pharma_treatmentTime_Flumazenil = 2;
+force force kat_pharma_treatmentTime_Ketamine = 3.5;
+force force kat_pharma_treatmentTime_Lidocaine = 3.5;
+force force kat_pharma_treatmentTime_Lorazepam = 2;
+force force kat_pharma_treatmentTime_Nalbuphine = 3.5;
+force force kat_pharma_treatmentTime_Naloxone = 3.5;
+force force kat_pharma_treatmentTime_Nitroglycerin = 3.5;
+force force kat_pharma_treatmentTime_Norepinephrine = 3.5;
+force force kat_pharma_treatmentTime_Phenylephrine = 3.5;
+force force kat_pharma_treatmentTime_Reorientation = 2;
+force force kat_pharma_treatmentTime_TXA = 3.5;
+
+// KAT - ADV Medical: Surgery
+force force kat_surgery_BreathCheck_MedLevel = 0;
+force force kat_surgery_BreathCheck_Time = 2;
+force force kat_surgery_closedLocation = 0;
+force force kat_surgery_closedReduction_MedLevel = 0;
+force force kat_surgery_closedTime = 5;
+force force kat_surgery_compoundChance = 30;
+force force kat_surgery_debrideTime = 10;
+force force kat_surgery_enable_fracture = true;
+force force kat_surgery_enable_selfCheckFracture = 1;
+force force kat_surgery_etomidateTime = 45;
+force force kat_surgery_fractureCheck_MedLevel = 1;
+force force kat_surgery_fractureCheck_Time = 10;
+force force kat_surgery_incisionTime = 5;
+force force kat_surgery_intermediateTime = 4;
+force force kat_surgery_npwtTime = 10;
+force force kat_surgery_openTime = 7.5;
+force force kat_surgery_simpleChance = 60;
+force force kat_surgery_surgicalAction_MedLevel = 0;
+force force kat_surgery_surgicalLocation = 0;
+force force kat_surgery_uncon_requieredForActions = false;
