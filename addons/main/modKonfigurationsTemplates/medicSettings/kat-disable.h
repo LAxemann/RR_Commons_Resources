0a5c4539cba5055d1478d757f7da8899a8e18a0c
Wasserstoff <41219647+GermanHydrogen@users.noreply.github.com>
2023-02-01 22:09:37 +0100

    Added settings to disable kat features

---
+++ addons/main/modKonfigurationsTemplates/medicSettings/kat-disable.h
@@ -0,0 +1,20 @@
+/**
+ * Should disable all kat 
+*/
+
+force force kat_airway_enable = false;
+
+// KAT - ADV Medical: Breathing
+force force kat_breathing_advPtxEnable = false;
+force force kat_breathing_enable = false;
+
+force force kat_breathing_enableCyanosis = false;
+
+force force kat_circulation_enable = false;
+force force kat_circulation_enable_CPR_Chances = false;
+
+force force kat_misc_enable = false;
+
+force force kat_pharma_IVdropEnable = false;
+
+force force kat_surgery_enable_fracture = false;
